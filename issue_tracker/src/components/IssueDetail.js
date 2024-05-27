8ee4e2e3606d40af7fca572e60799d745b8a2968
minsik <ppp37686@gmail.com>
2024-05-27 18:27:07 +0900

    Update IssueDetail.js : 이슈 디테일 창 높이 조절, 닫기 버튼 위치 조절

--- issue_tracker/src/components/IssueDetail.js
+++ issue_tracker/src/components/IssueDetail.js
@@ -45,8 +45,8 @@ const IssueDetail = ({ issue, onClose }) => {
 
   return (
     <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center">
-      <div className="bg-white p-6 rounded shadow-lg w-3/4 relative h-[50vh] max-h-[50vh] overflow-y-auto">
-        <button className="absolute top-2 right-2 text-gray-500 hover:text-gray-700" onClick={onClose}>
+      <div className="bg-white p-6 rounded shadow-lg w-3/4 relative h-[70vh] max-h-[70vh] overflow-y-auto">
+        <button className="absolute top-2 right-4 text-gray-500 hover:text-gray-700" onClick={onClose}>
           &times;
         </button>
         <div className="mb-4">
