38c6ff577075ea79fe586a934053f47082d725b8
Minsik Kim <ppp37686@gmail.com>
2024-05-31 05:22:02 +0900

    Update Header.js : Header font size up

--- issue_tracker/src/components/Header.js
+++ issue_tracker/src/components/Header.js
@@ -8,7 +8,7 @@ const Header = ({ isLoggedIn, setIsLoggedIn }) => {
 
   return (
     <div className="p-4 bg-gray-800 text-white flex justify-between items-center">
-      <h1 className="text-lg font-bold">Issue Tracker</h1>
+      <h1 className="text-xl font-bold">Issue Tracker</h1>
       {isLoggedIn && (
         <button
         className="text-white hover:text-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 flex items-center"
