9af2f5990269667738ddbfed730424ebaddf9503
Minsik Kim <ppp37686@gmail.com>
2024-05-31 03:52:11 +0900

    Added IssueStatistics.js : 통계 컴포넌트 파일 추가

---
+++ issue_tracker/src/components/IssueStatistics.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const IssueStatistics = ({ stats }) => {
+  return (
+    <div className="p-4 border rounded-lg shadow-md">
+      <h2 className="text-xl font-bold mb-4">Issue Statistics</h2>
+    </div>
+  );
+};
+
+export default IssueStatistics;
