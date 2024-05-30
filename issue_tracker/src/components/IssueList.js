458d2ddd059733f61a2ef92fa13577813bbf4857
Minsik Kim <ppp37686@gmail.com>
2024-05-31 03:07:56 +0900

    Update IssueList.js : New issue 버튼 추가

--- issue_tracker/src/components/IssueList.js
+++ issue_tracker/src/components/IssueList.js
@@ -123,6 +123,12 @@ const IssueList = ({ project, onSelectIssue }) => {
             >
               Search
             </button>
+            <button
+              className="text-white bg-green-600 hover:bg-green-500 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 ml-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+              onClick={handleSearch}
+            >
+              New issue
+            </button>
           </div>
         )}
       </div>
