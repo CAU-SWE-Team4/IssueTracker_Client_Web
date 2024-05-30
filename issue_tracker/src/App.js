597eb501fd3394c1244a1d0b9adc53845675a695
Minsik Kim <ppp37686@gmail.com>
2024-05-31 04:53:33 +0900

    Update App.js : 로그인 및 회원가입 컴포넌트 추가

--- issue_tracker/src/App.js
+++ issue_tracker/src/App.js
@@ -3,10 +3,14 @@ import ProjectList from './components/ProjectList';
 import IssueList from './components/IssueList';
 import IssueDetail from './components/IssueDetail';
 import Header from './components/Header';
+import Login from './components/Login';
+import Register from './components/Register';
 
 function App() {
   const [selectedProject, setSelectedProject] = useState(null);
   const [selectedIssue, setSelectedIssue] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isRegistering, setIsRegistering] = useState(false);
 
   const renderMainContent = () => {
     if (selectedIssue) {
@@ -21,14 +25,24 @@ function App() {
   return (
     <div className="flex flex-col">
       <Header />
-      <div className="flex flex-row">
-        <div className="w-1/5 max-w-sm h-screen border-r">
-          <ProjectList onSelectProject={setSelectedProject} />
+      {!isLoggedIn ? (
+        <div className="flex justify-center items-center h-screen">
+          {isRegistering ? (
+            <Register onRegister={() => setIsRegistering(false)} onCancel={() => setIsRegistering(false)} />
+          ) : (
+            <Login onLogin={() => setIsLoggedIn(true)} onShowRegister={() => setIsRegistering(true)} />
+          )}
         </div>
-        <div className="w-4/5 h-screen">
-          {renderMainContent()}
-        </div>        
-      </div>
+      ) : (
+        <div className="flex flex-row">
+          <div className="w-1/5 max-w-sm h-screen border-r">
+            <ProjectList onSelectProject={setSelectedProject} />
+          </div>
+          <div className="w-4/5 h-screen">
+            {renderMainContent()}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
