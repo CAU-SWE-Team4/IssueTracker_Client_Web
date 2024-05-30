240187a8fee0b5df051cf981a0e72c32c4d11517
Minsik Kim <ppp37686@gmail.com>
2024-05-31 05:06:52 +0900

    Update Header.js : Logout 버튼 추가

--- issue_tracker/src/components/Header.js
+++ issue_tracker/src/components/Header.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { RiLogoutBoxRLine } from "react-icons/ri";
 
-const Header = () => (
-  <div className="p-4 bg-gray-800 text-white">
-    <h1 className="text-lg font-bold">Issue Tracker</h1>
-  </div>
-);
+const Header = ({ isLoggedIn, setIsLoggedIn }) => {
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  };
 
+  return (
+    <div className="p-4 bg-gray-800 text-white flex justify-between items-center">
+      <h1 className="text-lg font-bold">Issue Tracker</h1>
+      {isLoggedIn && (
+        <button
+        className="text-white hover:text-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 flex items-center"
+        onClick={handleLogout}
+      >
+        <RiLogoutBoxRLine className="mr-2" size={20} />
+        Logout
+      </button>
+      )}
+    </div>
+  );
+};
 export default Header;
